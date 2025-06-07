cf71e52d5aa578977c964c776fe258173aff2177
0114444777771 <am6887744466@gmail.com>
2025-06-07 23:15:00 +0300

    Update CheckoutPage.jsx

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useCart } from "@/contexts/CartContext";
-import { db } from "@/src/firebase";
+import { db } from "@/firebase";
 import { addDoc, collection, updateDoc, doc, getDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import emailjs from '@emailjs/browser';
