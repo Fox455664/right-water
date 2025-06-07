9afc22820b64bfe752d4b107e7fd9fcbca0a18a6
0114444777771 <am6887744466@gmail.com>
2025-06-07 19:36:51 +0300

    Update CheckoutPage.jsx

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { CartContext } from "../context/CartContext";
+import { CartContext } from "../contexts/CartContext";
 import { db } from "../firebase"; // تأكد من مسار إعداد Firebase عندك
 import { collection, addDoc, updateDoc, doc, getDoc } from "firebase/firestore";
 import emailjs from "@emailjs/browser";
